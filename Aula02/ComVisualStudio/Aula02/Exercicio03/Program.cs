c3a855752566e23500a52dfaeeb0abe2f590d454
Talita <talis_phn2@hotmail.com>
2018-10-04 21:28:44 -0300

    Exemplos e Exercícios da Aula 02

---
+++ Aula02/ComVisualStudio/Aula02/Exercicio03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/* Exercício 01 - Faça um projeto (Console Aplication) com as seguintes atribuições: 
+ 1. Receba um número inteiro do usuário em uma variável "numero".
+ 2. Faça uma cópia dessa variável para a variável "numeroCopiado". (Passando a váriavel "numero" por valor)
+ 3. Calcule o dobro da variável "numeroCopiado" e guarde nele mesmo (Lembre-se que qualquer operação entre inteiros precisa ser guardada em uma varíavel)
+ 4. Imprima as duas varíaveis na tela.
+ */
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número. ");
+            int numero = Convert.ToInt32(Console.ReadLine()); //converter a captura de string para int
+
+            int numerocopiado = numero *2 ;
+
+            Console.WriteLine($"O valor da variável número é: {numero}.");
+            Console.WriteLine($"O valor da variável númerocopiado é: {numerocopiado}.");
+
+        }
+    }
+}
---
+++ Aula02/ComVisualStudio/Aula02/Exercicio04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/*Exercício 02 - Faça segundo projeto (Console Aplication) com as seguintes atribuições: 
+ 1. Receba um número inteiro do usuário em uma variável "numero".
+ 2. Faça uma cópia dessa variável para a variável "numeroCopiado". (Passando a váriavel "numero" por referência)
+ 3. Calcule o dobro da variável "numeroCopiado" e guarde nele mesmo (Lembre-se que qualquer operação entre inteiros precisa ser guardada em uma varíavel)
+ 4. Imprima as duas varíaveis na tela.*/
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um númemro");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            ref int numerocopiado = ref numero;
+            numerocopiado = numerocopiado * 2;
+
+            Console.WriteLine($"O valor da variável numero é: {numero}");
+            Console.WriteLine($"O valor da variável numerocopiado é: {numerocopiado}");
+        }
+    }
+}

fdca2d0d1c09f13b5f451a6812f95b81234ac238
Talita <talis_phn2@hotmail.com>
2018-10-04 23:10:23 -0300

    Atualização do Read

---
+++ Aula02/Aula02/Exercicio01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome.");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Seja bem vindo(a) {nome}, bem vindo novamente {nome}!!!");
+            Console.ReadKey();
+        }
+    }
+}

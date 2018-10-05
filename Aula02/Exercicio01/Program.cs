4e642f3ed3332ec14a0ee150b68bad760d46bca7
Thiago Borges Amaral <tborgesa@gmail.com>
2018-10-05 08:12:58 -0300

    Exemplos feitos na aula 2

---
+++ Aula02/Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plataforma .NET");
+
+            Console.WriteLine("Digite seu nome.");
+            string nome = Console.ReadLine();
+
+            //Três formas de montar string e apresentar pawra o usuário.
+            Console.WriteLine("Seja bem vindo, " + nome + ".");
+            Console.WriteLine(string.Format($"Seja bem vindo, {0}.", nome));
+            Console.WriteLine($"Seja bem vindo, {nome}.");
+
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ Aula02/Exercicio2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://www.macoratti.net/17/01/cshp_primit1.htm
+            Console.WriteLine("VALOR X REFERENCIA");
+
+            int numeroPorValor = 5;
+            int numero2PorValor = numeroPorValor;
+            numeroPorValor = 2;
+
+            Console.WriteLine($"Número por Valor: {numeroPorValor}.");
+            Console.WriteLine($"Número por Valor 2: {numero2PorValor}.");
+            Console.WriteLine("***************************");
+
+            int valorPorReferencia = 5;
+            ref int valor2PorReferencia = ref valorPorReferencia;
+            valor2PorReferencia = 2;
+
+            Console.WriteLine($"Número por Referência: {valorPorReferencia}.");
+            Console.WriteLine($"Número por Referência 2: {valor2PorReferencia}.");
+            Console.WriteLine("***************************");
+
+            Thiago thiago = new Thiago();
+            thiago.Nome = "Thiago";
+
+            Thiago thiago2 = thiago;
+            thiago2.Nome = "Mudou!";
+
+            Console.WriteLine($"Classe Thiago: {thiago.Nome}.");
+            Console.WriteLine($"Classe Thiago2: {thiago2.Nome}.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Thiago
+    {
+        public string Nome;
+    }
+}

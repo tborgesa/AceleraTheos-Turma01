6630e384911b84822bc57dfec6e22b7cc2d20216
Rafael Rodrigues <rafaelrodrigues446@gmail.com>
2018-10-09 20:09:51 -0300

    Aula03

---
+++ Aula03/ConversaoTipoNumericos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversaoTipoNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Conversão de tipos integrais");
+
+            short tipoShort = 256;
+            byte tipoByte = (byte)tipoShort;
+
+            Console.WriteLine($"tipo short é {tipoShort} e em byte ficou {tipoByte}.");
+            Console.ReadKey();*/
+
+
+            Console.WriteLine("Conversão de tipos flutuantes");
+            double tipoDouble = 80.76;
+            int tipoInt = (int)tipoDouble;
+
+            Console.WriteLine($"Tipo double é {tipoDouble} e em int ficou {tipoInt}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula03/OperacoesTiposNumericos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacoesTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 10 + 5;
+            int subtracao = 10 - 5;
+            int multiplacao = 10 * 5;
+            int divisao = 10 / 5;
+            int resto = 10 % 5;
+
+            int incrementalAntes = 1;
+            int incrementalDepois = 1;
+
+            Console.WriteLine($"Antes : {++incrementalAntes}");//incrementa no valor antes e retorna o valor incrementado
+            Console.WriteLine($"Depois : {incrementalDepois++}");//incrementar o valor depois de mandar o retorno, ou seja retorna o valor 1 
+
+            Console.WriteLine($"Antes : {incrementalAntes}");//valor incrementado 2
+            Console.WriteLine($"Depois : {incrementalDepois}");//valor incrementado 2
+
+            Console.ReadKey();
+
+
+
+            /*---------------------------------------------------------------*/
+            //Biblioteca MATH
+            double bibliotecaMath = Math.Round(56.87);
+            Console.WriteLine($"Arredondamento: {bibliotecaMath}");
+
+            Console.ReadKey();
+
+        }
+    }
+}

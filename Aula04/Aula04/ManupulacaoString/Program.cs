881a941b331222a3d495f12465f6c590b5cc15f4
Thiago Borges Amaral <tborgesa@gmail.com>
2018-10-11 19:29:51 -0300

    Exemplos ad Aula04

--- Aula04/Aula04/ManupulacaoString/Program.cs
+++ Aula04/Aula04/ManupulacaoString/Program.cs
@@ -36,21 +36,21 @@ namespace ManupulacaoString
 
             Console.WriteLine($"Sobrenome: {sobrenome}");
 
+            //https://docs.microsoft.com/pt-br/dotnet/standard/base-types/standard-numeric-format-strings
             Console.WriteLine("TO STRING - FORMATAÇÃO");
 
             decimal valor = 100_000_000_000;
 
             int numero = 9;
-
+            
             Console.WriteLine($"Valor: {valor.ToString("c")}");
             Console.WriteLine($"Valor: {valor.ToString("c", new CultureInfo("en-US"))}");
             Console.WriteLine($"Numero: {numero.ToString("d2")}");
             Console.WriteLine($"Valor: {valor.ToString("n2", new CultureInfo("en-US"))}");
 
             Console.WriteLine($"Valor: {valor:c}");
-            Console.WriteLine($"Valor: {valor.ToString("c", new CultureInfo("en-US"))}");
-            Console.WriteLine($"Numero: {numero.ToString("d2")}");
-            Console.WriteLine($"Valor: {valor.ToString("n2", new CultureInfo("en-US"))}");
+            Console.WriteLine($"Numero: {numero:d2}");
+            Console.WriteLine($"Valor: {valor:n2}");
 
             Console.ReadKey();
         }

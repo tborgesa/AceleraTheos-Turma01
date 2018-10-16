69557d19dc4fc1b83b19586b976d537ba852d515
Caroline Molina <carol.molinah@hotmail.com>
2018-10-16 13:14:53 -0300

    Exercício 05

---
+++ Aula04/Exercicio05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filme, livro, hobby;
+
+            Console.WriteLine("Filme favorito?");
+            filme = Console.ReadLine();
+
+            Console.WriteLine("Livro de cabeceira?");
+            livro = Console.ReadLine();
+
+            Console.WriteLine("Hobby preferido?");
+            hobby = Console.ReadLine();
+    
+            Console.WriteLine($"Filme: {filme.ToUpper()}, Livro: {livro.ToUpper()}, Hobby: {hobby.ToUpper()}");
+            Console.ReadKey();
+        }
+    }
+}

b2e9e03dc20de8fc60aae760f8f2c514eab3f9f0
Aline <aline.estercio@theos.com.br>
2018-10-16 18:34:40 -0300

    alterações exercício 5

--- Aula4/Aula4/Exercicio5/Program.cs
+++ Aula4/Aula4/Exercicio5/Program.cs
@@ -6,19 +6,23 @@ namespace Exercicio5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Por favor, informe seu filme favorito.");
+            Console.WriteLine("Por favor, informe seu nome.");
+            string nome = Console.ReadLine();
+
+
+            Console.WriteLine($"Olá {nome}! por favor, informe seu filme favorito.");
             string filme = Console.ReadLine();
             filme = filme.ToUpper();
 
-            Console.WriteLine("Por favor, informe seu livro de cabeceira.");
+            Console.WriteLine($"Por favor, informe seu livro de cabeceira.");
             string livro = Console.ReadLine();
             livro = livro.ToUpper();
 
-            Console.WriteLine("Por favor, informe seu hobby preferido.");
+            Console.WriteLine($"Por favor, informe seu hobby preferido.");
             string hobby = Console.ReadLine();
-            livro = livro.ToUpper();
+            hobby = hobby.ToUpper();
 
-            Console.WriteLine($"Filme :{filme}, Livro: {livro}, Hobby: {hobby}.");
+            Console.WriteLine($"{nome}, o seu filme favorito é: {filme}, o livro: {livro}, e seu hobby: {hobby}.");
 
             Console.ReadKey();
 

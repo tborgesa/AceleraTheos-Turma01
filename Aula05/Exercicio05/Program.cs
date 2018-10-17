2b0e07215c32bfcede173a245b65331f7c8d05f1
Rafael Rodrigues <rafaelrodrigues446@gmail.com>
2018-10-17 08:30:59 -0300

    Exercicios completos Aula05

--- Aula05/Exercicio05/Program.cs
+++ Aula05/Exercicio05/Program.cs
@@ -14,16 +14,17 @@ namespace Exercicio05
             //Solicite a data de filtro para o usuário e retorno a data de inicio e data de fim que é 
             //capaz de buscar todas as transações da data solicitada.
 
-            DateTime dataHoraTransacao = DateTime.Now;
-            DateTime dataInicial = new DateTime();
-            DateTime dataFinal = new DateTime();
+           
+            DateTime dataPesquisa = new DateTime();
 
             Console.WriteLine($"Digite a data inicial para a pesquisa");
-            dataInicial = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine($"Digite a data final para a pesquisa");
-            dataFinal = DateTime.Parse(Console.ReadLine());
+            dataPesquisa = DateTime.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{dataHoraTransacao}");
+            DateTime dataInicial = dataPesquisa;
+            DateTime dataFinal = dataPesquisa.AddHours(24).AddMilliseconds(-1);
+
+            Console.WriteLine($"{dataInicial:dd/MM/yyyy HH:mm:ss}");
+            Console.WriteLine($"{dataFinal:dd/MM/yyyy HH:mm:ss}");
 
             Console.ReadKey();
         }

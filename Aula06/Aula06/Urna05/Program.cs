629391083424c31deac5053a09c3b7553c5a5ab9
Caroline Molina <carol.molinah@hotmail.com>
2018-10-21 11:56:23 -0300

    Exercício 04 da Aula06

---
+++ Aula06/Aula06/Urna05/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urna05
+{
+    class Program
+    {
+        /*4. Faça um sistema de pesquisa eleitoral para presidente no segundo turno.
+                       Requisitos:
+                       1. O usuário pode escolher 13 - Haddad / 17- Bolsonaro / 00 - Brancos 
+                       2. Qualquer outra escolha anulará o voto na pesquisa
+                       3. O usuário poderá ter a escolha de votar quantas vezes ele quiser 
+                       (simulando várias pessoas votando) ou ver o resultado ou sair;
+                       4. O resultado deverá conter Votos Totais, Votos Válidos 
+                       (Votos excluindos os brancos e nulos). % de votos no Haddad e % de votos do Bolsonaro.
+                       5. Depois de ver o resultado o usuário poderá voltar para a "urna" e continuar votando ou sair do sistema.*/
+
+        static string _mensagemInvalido = "Entrada inválida, execute o sistema e tente novamente!";
+        static void Main(string[] args)
+        {
+            Urna05();
+            Console.ReadKey();
+        }
+
+        private static void Urna05()
+        {
+            int votosHaddad = 0;
+            int votosBolsonaro = 0;
+            int votosBrancos = 0;
+            int votosNulos = 0;
+            int votosTotal = votosHaddad + votosBolsonaro + votosBrancos;
+            int votosValidos = votosTotal - (votosBrancos + votosNulos);
+
+            bool saiu = false;
+            while (!saiu)
+            {
+                Console.Clear();
+                string menu = @"Escolha uma das opções
+01 - Digite 1 para votar
+02 - Digite 2 para ver resultados
+03 - Digite 3 para sair";
+                Console.WriteLine(menu);
+
+                string numeroDigitado = Console.ReadLine();
+                if (!int.TryParse(numeroDigitado, out var numero))
+                {
+                    Console.WriteLine(_mensagemInvalido);
+                    return;
+                }
+
+                switch (numero)
+                {
+                    case 1:
+                        Console.WriteLine(@"Escolha uma opção para votar: 
+13 - Haddad
+17 - Bolsonaro
+00 - Brancos
+Atenção: qualquer escolha diferente das opções acima será considerada nulo");
+
+                        string voto = Console.ReadLine();
+
+                        if (voto == "13")
+                            votosHaddad += 1;
+                        else if (voto == "17")
+                            votosBolsonaro += 1;
+                        else if (voto == "00")
+                            votosBrancos += 1;
+                        else
+                            votosNulos += 1;
+                        break;
+
+                    case 2:
+                        string apuracao = $@"Votos totais: {votosTotal}
+Votos brancos: {votosBrancos}
+Votos nulos: {votosNulos}
+Votos Haddad: {(votosHaddad + votosTotal) * 100}%
+Votos Bolsonado: {(votosBolsonaro + votosTotal) * 100}%";
+
+                        Console.WriteLine("Resultado da apuração eleições 2018");
+                        Console.WriteLine(apuracao);
+                        Console.WriteLine("Digite 1 para votar novamente ou qualquer tecla para sair");
+                        string fim = Console.ReadLine();
+
+                        if (fim == "1")
+                            goto case 1;
+                        else
+                            Console.WriteLine("Saindo da votação!");
+                        break;
+                    case 3:
+                        Console.WriteLine("Você escolheu sair");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+        }
+    }
+}

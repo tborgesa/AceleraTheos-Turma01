7aa12d7a013bc65ab04fec99274609a289ef64d8
Caroline Molina <carol.molinah@hotmail.com>
2018-10-21 12:06:20 -0300

    Remoção de comentário

--- Aula06/Aula06/Urna05/Program.cs
+++ Aula06/Aula06/Urna05/Program.cs
@@ -8,15 +8,6 @@ namespace Urna05
 {
     class Program
     {
-        /*4. Faça um sistema de pesquisa eleitoral para presidente no segundo turno.
-                       Requisitos:
-                       1. O usuário pode escolher 13 - Haddad / 17- Bolsonaro / 00 - Brancos 
-                       2. Qualquer outra escolha anulará o voto na pesquisa
-                       3. O usuário poderá ter a escolha de votar quantas vezes ele quiser 
-                       (simulando várias pessoas votando) ou ver o resultado ou sair;
-                       4. O resultado deverá conter Votos Totais, Votos Válidos 
-                       (Votos excluindos os brancos e nulos). % de votos no Haddad e % de votos do Bolsonaro.
-                       5. Depois de ver o resultado o usuário poderá voltar para a "urna" e continuar votando ou sair do sistema.*/
 
         static string _mensagemInvalido = "Entrada inválida, execute o sistema e tente novamente!";
         static void Main(string[] args)

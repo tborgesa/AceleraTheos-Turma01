cbd699aa148f9e6e0d55eb61126b9b4b3a3943ce
Caroline Molina <carol.molinah@hotmail.com>
2018-10-21 10:34:29 -0300

    Exercício 03 da Aula 06

--- Aula06/Aula06/Exercicio03/Program.cs
+++ Aula06/Aula06/Exercicio03/Program.cs
@@ -5,9 +5,48 @@ namespace Exercicio03
 {
     class Program
     {
+        static string _mensagemInvalido = "Entrada inválida, execute o sistema e tente novamente!";
         static void Main(string[] args)
         {
-            //preciso ter uma variavel para cada intervalo, menor que 100, entre 100 e 200 e maior que 200
+            /*3. Solicite ao usuário 10 números e no final diga quantos números são menores que 100, quantos estão entre 100 e 200 e quantos são maiores que 200.*/
+
+            VerificaNumero();
+            Console.ReadKey();
         }
+
+        private static void VerificaNumero()
+        {
+            string numeroDigitado;
+            int intervaloMenor = 0;
+            int intervaloMediano = 0;
+            int intervaloMaior = 0;
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Digite um número");
+                numeroDigitado = Console.ReadLine();
+
+                if (!int.TryParse(numeroDigitado, out var numero))
+                {
+                    Console.WriteLine(_mensagemInvalido);
+                    return;
+                }
+
+                if (numero < 100)
+                    intervaloMenor += 1;
+                else if (numero > 100 && numero < 201)
+                    intervaloMediano += 1;
+                else if (numero > 200)
+                    intervaloMaior += 1;
+            }
+
+            Console.WriteLine($"Foram digitados {intervaloMenor} menores que 100, {intervaloMediano} entre 100 e 200 e {intervaloMaior} maiores que 200.");
+
+            Console.ReadKey();
+
+        }
+
     }
 }
+

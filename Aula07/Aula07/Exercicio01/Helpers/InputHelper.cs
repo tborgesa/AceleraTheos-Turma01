4149f941a453274e6a3efabe4b5ed5a3082d4017
Caroline Molina <carol.molinah@hotmail.com>
2018-10-25 13:42:55 -0300

    Exercício 01, não tá funcionando ainda

---
+++ Aula07/Aula07/Exercicio01/Helpers/InputHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01.Helpers
+{
+    public class InputHelper
+    {
+        public static double GetInputDouble(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+
+                string numeroDigitado = Console.ReadLine();
+
+                if (!double.TryParse(numeroDigitado, out var numero))
+                {
+                    MensagemUsuario(mensagemInvalida);
+                }
+                else
+                    return numero;
+            }
+        }
+
+        public static string GetInputString(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+
+                string informacaoDigitada = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(informacaoDigitada))
+                {
+                    MensagemUsuario(mensagemInvalida);
+                }
+                else
+                    return informacaoDigitada;
+            }
+        }
+        public static void MensagemUsuario(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula07/Aula07/Exercicio01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exercicio01.Helpers;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static string nome, matricula = "";
+        static double peso, altura = 0.0;
+        private static string _mensagemInvalida = "Entrada inválida, tente novamente.";
+        static void Main(string[] args)
+        {
+            InserirAluno();
+        }
+
+        private static void InserirAluno()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+
+                nome = InputHelper.GetInputString("Digite o nome: ", _mensagemInvalida);
+                matricula = InputHelper.GetInputString("Digite a matricula: ", _mensagemInvalida);
+                peso = InputHelper.GetInputDouble("Digite o peso: ", _mensagemInvalida);
+                altura = InputHelper.GetInputDouble("Digite a altura: ", _mensagemInvalida);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($@"Nome: {nome}
+Matrícula: {matricula}
+Peso: {peso:N2}kg
+Altura: {altura}");
+            Console.ReadKey();
+        }
+
+    }
+}

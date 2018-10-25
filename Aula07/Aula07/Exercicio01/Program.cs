835770e77de3cb9ab2dae2fc0663ad60357821ab
Caroline Molina <carol.molinah@hotmail.com>
2018-10-25 17:39:14 -0300

    Algumas coisas do exercício 01

--- Aula07/Aula07/Exercicio01/Program.cs
+++ Aula07/Aula07/Exercicio01/Program.cs
@@ -8,9 +8,12 @@ namespace Exercicio01
         static string nome, matricula = "";
         static double peso, altura = 0.0;
         private static string _mensagemInvalida = "Entrada inválida, tente novamente.";
+        static double _pesoGuardado = 0.0;
+        static double _alturaGuardada = 0.0;
         static void Main(string[] args)
         {
             InserirAluno();
+            CalcularAltura();
         }
 
         private static void InserirAluno()
@@ -23,6 +26,10 @@ namespace Exercicio01
                 peso = InputHelper.GetInputDouble("Digite o peso: ", _mensagemInvalida);
                 altura = InputHelper.GetInputDouble("Digite a altura: ", _mensagemInvalida);
                 Console.WriteLine();
+
+                ValidarPeso();
+                ValidarAltura();
+
             }
 
             Console.WriteLine($@"Nome: {nome}
@@ -32,5 +39,25 @@ Altura: {altura}");
             Console.ReadKey();
         }
 
+        private static void ValidarPeso()
+        {
+            if (_pesoGuardado < peso)
+                _pesoGuardado += peso;
+        }
+
+        private static void ValidarAltura()
+        {
+            if (_alturaGuardada < altura)
+                _alturaGuardada += peso;
+        }
+        private static void CalcularAltura()
+        {
+            double resultadoAltura = 0.0;
+            ValidarAltura();
+
+
+            Console.WriteLine($"Resultado: {_pesoGuardado} e {_alturaGuardada}");
+            Console.ReadKey();
+        }
     }
 }

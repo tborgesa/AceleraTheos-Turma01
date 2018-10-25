5c907dc8af66f428bb8636fa4bad67be7d643752
Talita <talis_phn2@hotmail.com>
2018-10-24 23:45:58 -0300

    Exercícios Aula 07 - incompleto.

---
+++ Aula07/Exercicios/Exercicio01/Helpers/InputHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio01.Helpers
+{
+    class InputHelper
+    {
+        public static int GetInputInt(string mensagemInvalida, bool limparTela = true)
+        {
+
+            while (true)
+            {
+                if (limparTela)
+                    Console.Clear();
+
+                if (!int.TryParse(Console.ReadLine(), out int numero))
+                    MensagemUsuario(mensagemInvalida);
+                else
+                    return numero;
+            }
+        }
+
+        public static double GetInputDouble(string mensagemInvalida, bool limparTela = true)
+        {
+            while (true)
+            {
+                if (limparTela)
+                    Console.Clear();
+
+                if (!double.TryParse(Console.ReadLine(), out double numero))
+                    MensagemUsuario(mensagemInvalida);
+                else
+                    return numero;
+            }
+        }
+
+        public static void MensagemUsuario(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula07/Exercicios/Exercicio01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Exercicio01.Helpers;
+/*1) Monte um algoritimo que le 10 dados de alunos: Nome, Numero Matricula, Peso, Altura.
+No final apresente:
+- Aluno(Matricula - Nome) com maior altura.
+- Aluno(Matricula - Nome) com menor altura.
+- Aluno(Matricula - Nome) com maior peso.
+- Aluno(Matricula - Nome) com menor peso.
+- Média do peso
+- Média da altura */
+namespace Exercicio01
+{
+    class Program
+    {
+        static double _matricula;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicío 01 - Matrículas");
+            Console.WriteLine();
+
+            Console.WriteLine("Informe o nome do aluno:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o número da matrícula do aluno:");
+            double matricula = InputGetDouble();
+
+            Console.WriteLine("Informe o peso do aluno: ");
+            double peso = InputGetDouble();
+
+            Console.WriteLine($"Nome: {nome} - Matricula: {matricula} - Peso: {peso}");
+            Console.ReadKey();
+        }
+
+        public static double InputGetDouble()
+        {
+            while (true)
+            {
+                if (!double.TryParse(Console.ReadLine(), out double numero))
+                {
+                    Console.WriteLine("Você digitou um valor inválido. Tente novamente!");
+                    Console.ReadKey();
+                }
+                else
+                    return numero;
+            }
+        }
+    }
+}

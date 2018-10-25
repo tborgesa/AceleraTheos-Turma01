d7bf372ac1a94089926ffecfd3143c254253b67e
Caroline Molina <carol.molinah@hotmail.com>
2018-10-25 20:40:08 -0300

    Estrutura básica do exercício 02 (Menu principal)

---
+++ Aula07/Aula07/Exercicio02/Helpers/InputHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace Exercicio01.Helpers
+{
+    public class InputHelper
+    {
+        public static int GetInputInt(string texto, string mensagemInvalido, bool limparTela = true)
+        {
+            while (true)
+            {
+                if (limparTela)
+                    Console.Clear();
+
+                Console.WriteLine(texto);
+
+                string numeroDigitado = Console.ReadLine();
+
+                if (!int.TryParse(numeroDigitado, out var numero))
+                {
+                    MensagemUsuario(mensagemInvalido);
+                }
+                else
+                    return numero;
+            }
+        }
+        public static double GetInputDouble(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+
+                string numeroDigitado = Console.ReadLine();
+
+                if (!double.TryParse(numeroDigitado, out var numero))
+                {
+                    MensagemUsuario(mensagemInvalida);
+                }
+                else
+                    return numero;
+            }
+        }
+
+        public static string GetInputString(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+
+                string informacaoDigitada = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(informacaoDigitada))
+                {
+                    MensagemUsuario(mensagemInvalida);
+                }
+                else
+                    return informacaoDigitada;
+            }
+        }
+        public static void MensagemUsuario(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula07/Aula07/Exercicio02/Program.cs
@@ -0,0 +1,70 @@
+﻿using Exercicio01.Helpers;
+using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static double _saldo = 0.0;
+        private static string _mensagemInvalida = "Entrada inválida, tente novamente.";
+        static void Main(string[] args)
+        {
+            MenuPrincipal();
+           }
+
+        private static void MenuPrincipal()
+        {
+            while (true)
+            {
+                string menu = @"
+Escolha uma opção abaixo:
+01 - Consultar saldo
+02 - Saque
+03 - Depósito
+04 - Sair
+";
+                int opcao = InputHelper.GetInputInt(menu, _mensagemInvalida);
+                switch (opcao)
+                {
+                    case 01:
+                        ConsultaSaldo();
+                        break;
+                    case 02:
+                        Sacar();
+                        break;
+                    case 03:
+                        Depositar();
+                        break;
+                    case 04:
+                        InputHelper.MensagemUsuario("Saindo do sistema!");
+                        return;
+                    default:
+                        InputHelper.MensagemUsuario(_mensagemInvalida);
+                        break;
+                }
+            }
+
+        }
+
+        private static void Depositar()
+        {
+            Console.Clear();
+            InputHelper.MensagemUsuario("Você está no menu depositar!");
+      
+        }
+
+        private static void Sacar()
+        {
+            Console.Clear();
+            InputHelper.MensagemUsuario("Você está no menu sacar!");
+       
+        }
+
+        private static void ConsultaSaldo()
+        {
+            Console.Clear();
+            InputHelper.MensagemUsuario("Você está no menu consultar saldo!");
+         
+        }
+    }
+}

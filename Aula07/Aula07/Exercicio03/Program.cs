077d5384ec45c0de56608c68eeba008f8921e012
Caroline Molina <carol.molinah@hotmail.com>
2018-10-25 21:25:50 -0300

    Exercício 03 incompleto

---
+++ Aula07/Aula07/Exercicio03/Program.cs
@@ -0,0 +1,29 @@
+﻿using Refatoracao.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        private static string _mensagemInvalida = "Entrada inválida, tente novamente.";
+        static void Main(string[] args)
+        {
+            int numero = InputHelper.GetInputInt("Digite um número", _mensagemInvalida);
+            Console.WriteLine($"{numero}! = {CalcularFatorial(numero)}");
+            Console.ReadKey();
+        }
+
+        static double CalcularFatorial(int numero)
+        {
+            if (numero <= 1)
+                return 1;
+            else
+                return numero * CalcularFatorial(numero - 1);
+        }
+
+    }
+}

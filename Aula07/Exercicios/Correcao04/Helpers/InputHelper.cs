bf7a56fb0088294884543eabb98867f7566210f5
Talita <talis_phn2@hotmail.com>
2018-10-29 19:19:19 -0300

    *Exercícios 07 finalizados + correção.*

---
+++ Aula07/Exercicios/Correcao04/Helpers/InputHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Correcao04.Helpers
+{
+    static class InputHelper
+    {
+        public static double GetInputDouble(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+                string numeroDigitado = Console.ReadLine();
+
+                if (!double.TryParse(numeroDigitado, out var numero))
+                {
+                    Console.WriteLine(mensagemInvalida);
+                    Console.ReadKey();
+                }
+                else
+                    return numero;
+            }
+        }
+
+        public static int GetInputInt(string texto, string mensagemInvalida)
+        {
+            while (true)
+            {
+                Console.WriteLine(texto);
+                string numeroDigitado = Console.ReadLine();
+
+                if (!int.TryParse(numeroDigitado, out var numero))
+                {
+                    Console.WriteLine(mensagemInvalida);
+                    Console.ReadKey();
+                }
+                else
+                    return numero;
+            }
+        }
+    }
+}
---
+++ Aula07/Exercicios/Correcao04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Correcao04.Helpers;
+
+namespace Correcao04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reprodução de Coelhos");
+
+            int coelhosFerteis = InputHelper.GetInputInt("Informe a quantidade de coelhos inciais: ", "Digite um número válido");
+            int coelhosDesejados = InputHelper.GetInputInt("Informe a quantidade de coelhos desejados: ", "Digite um número válido");
+            int coelhosInferteis = 0;
+            int coelhosTotais = coelhosFerteis + coelhosInferteis;
+            int mes = 0;
+
+            if (coelhosDesejados <= coelhosFerteis)
+            {
+                Console.WriteLine("Não é necessário reprodução, pois você já tem os coelhos desejados");
+                Console.ReadKey();
+            }
+
+            while (coelhosFerteis + coelhosInferteis < coelhosDesejados)
+            {
+                mes++;
+                int ferteisMesAnterior = coelhosFerteis;
+                int inferteisMesAnterior = coelhosInferteis;
+                coelhosInferteis = ferteisMesAnterior;
+                coelhosFerteis = ferteisMesAnterior + coelhosInferteis;
+            }
+
+            Console.WriteLine($"Você terá que esperar {mes} mes(es) para obter a quantidade desejada");
+            Console.ReadKey();
+        }
+    }
+}

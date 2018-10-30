c945a30d0c105eeeb37632ab2da57fc52a65d9be
Talita <talis_phn2@hotmail.com>
2018-10-29 21:28:02 -0300

    Aula 09 - Orientado Objeto

---
+++ Aula09/UaiQueijos.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UaiQueijos.Comum.Helpers;
+using UaiQueijos.Dominio;
+
+namespace UaiQueijos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputHelper.MensagemUsuario("Acessando diretamente um projeto Comum.");
+
+            Cliente cliente = new Cliente(new DateTime(1987, 07, 24), "07365412345", "Talita");
+            cliente.Endereco = "Rua Castro Alves, 1390. Maringá - PR";
+
+            Cliente cliente1 = cliente;
+
+            Console.ReadKey();
+        }
+    }
+}

1fdb9b2f8d77036f6c88c48c8d25beea5feff37a
Thiago <tborgesa@gmail.com>
2018-11-01 19:40:08 -0300

    Aula10

---
+++ Aula10/UaiQueijos/UaiQueijos.ComsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using UaiQueijos.Comum.Helpers;
+using UaiQueijos.Dominio;
+using System;
+using ClienteAlias = UaiQueijos.Dominio.Cliente.Cliente;
+
+namespace UaiQueijos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteAlias cliente = new ClienteAlias(
+                new DateTime(1987, 7, 8), 
+                "07346153618", 
+                "Thiago");
+
+            ClienteAlias outroCliente = new ClienteAlias(
+                new DateTime(1987, 7, 8),
+                "07346153618",
+                "Thiago",
+                "Rua Castro Alves, 1390. Maringá - PR");
+
+            var teste = cliente.Nome;
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula10/UaiQueijos/UaiQueijos.Dominio/Cliente/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UaiQueijos.Dominio.Cliente
+{
+    public class Cliente
+    {
+        public Cliente(DateTime dataNascimento, string cpf, string nome)
+        {
+            DataNascimento = dataNascimento;
+            Cpf = cpf;
+            Nome = nome;
+        }
+
+        public Cliente(DateTime dataNascimento, string cpf, string nome, string endereco)
+        {
+            DataNascimento = dataNascimento;
+            Cpf = cpf;
+            Nome = nome;
+            Endereco = endereco;
+        }
+
+        public string Nome { get; }
+        public string Cpf { get; }
+        public DateTime DataNascimento { get; }
+        public string Endereco { get; private set; }
+
+        public void AlterarEndereco(string endereco)
+        {
+            Endereco = endereco;
+        }
+    }
+}

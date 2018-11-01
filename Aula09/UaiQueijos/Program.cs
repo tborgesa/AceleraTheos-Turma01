6f7b769dc5766ce0d81cd050dd0f1f115c9041ba
Christian Castilho <chrcastilho@gmail.com>
2018-10-31 22:18:20 -0300

    Aula 09

---
+++ Aula09/UaiQueijos/Program.cs
@@ -0,0 +1,26 @@
+﻿using UaiQueijos.Comum.Helpers;
+using UaiQueijos.Dominio;
+using System;
+
+namespace UaiQueijos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente(
+                new DateTime(1987, 7, 8),
+                "07346153618",
+                "Thiago");
+
+            cliente.Endereco = "Rua Castro Alves, 1390. Maringá - PR";
+
+            Cliente cliente1 = cliente;
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

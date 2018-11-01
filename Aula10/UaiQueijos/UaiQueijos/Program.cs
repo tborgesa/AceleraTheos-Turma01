140e3a794b9cdff75cbb2e8266af08f3b940f042
Caroline Molina <carol.molinah@hotmail.com>
2018-11-01 20:49:48 -0300

    Aula 10

---
+++ Aula10/UaiQueijos/UaiQueijos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UaiQueijos.Comum.Helpers;
+using UaiQueijos.Dominio;
+using ClienteAlias = UaiQueijos.Dominio.Cliente.Cliente;
+
+namespace UaiQueijos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputHelper.MensagemUsuario("Acessando o comum");
+
+            ClienteAlias cliente = new ClienteAlias(new DateTime(1994, 7, 29), "42120768889", "Caroline", "Rua Urano, 229 - Jd Imperial II");
+         
+            Console.ReadKey();
+        }
+    }
+}

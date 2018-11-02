9126dfcfeb2befc99de351e3a3305ec6522a19c1
Christian A. F. Castilho <chrcastilho@gmail.com>
2018-11-01 21:29:39 -0300

    Aula10

---
+++ Aula10/TheosBiblioteca.Dominio/Cadastro/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheosBiblioteca.Dominio.Cadastro.Funcionario
+{
+    public class Funcionario
+    {
+        public Funcionario(string nome, string cpf, string setor)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Setor = setor;
+        }
+
+        public string Nome { get; }
+        public string Cpf { get; }
+        public string Setor { get; }
+    }
+}
---
+++ Aula10/TheosBiblioteca/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RegistroAutorALias = TheosBiblioteca.Dominio.Cadastro.Autor.Autor;
+
+namespace TheosBiblioteca.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Numero pode não ser obrigatorio
+            Console.WriteLine(@"Teste faça as inserções:
+Editora
+ Nome
+ Cnpj
+ Logradouro
+ Numero
+ Bairro
+ Cidade
+ Cep");
+
+
+            Console.WriteLine(@"
+Livro
+ Nome
+ Assunto
+ Editora
+ Autor");
+            Console.WriteLine(@"
+Funcionário
+ Nome
+ Cpf
+ Setor");
+
+            RegistroAutorALias autor = new RegistroAutorALias("Nome");
+
+            var teste = autor.Nome;
+
+            Console.WriteLine(teste);
+
+            Console.ReadKey();
+        }
+    }
+}

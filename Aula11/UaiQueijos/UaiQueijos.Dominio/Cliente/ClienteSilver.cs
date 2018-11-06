64c7a848b4dd9d44aea5377fa2addb509699e8a6
Thiago <tborgesa@gmail.com>
2018-11-06 21:14:43 -0200

    Aula 11

---
+++ Aula11/UaiQueijos/UaiQueijos.Dominio/Cliente/ClienteSilver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UaiQueijos.Dominio.Cliente
+{
+    public class ClienteSilver : Cliente
+    {
+        public ClienteSilver(DateTime dataNascimento, string cpf, string nome)
+         : base(dataNascimento, cpf, nome)
+        {
+
+        }
+
+        public ClienteSilver(DateTime dataNascimento, string cpf, string nome, string endereco)
+         : base(dataNascimento, cpf, nome)
+        {
+
+        }
+
+        public override double ObterDesconto(double descontoPadrao)
+        {
+            return base.ObterDesconto(descontoPadrao) * 1.5;
+        }
+
+        public void EspecificoSilver()
+        {
+
+        }
+
+    }
+}

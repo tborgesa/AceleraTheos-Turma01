4e47417ca278a69f31c637714db70b8671c8622d
Christian Castilho <chrcastilho@gmail.com>
2018-11-12 21:25:43 -0200

    Aula 12 - Done

---
+++ Theos.Biblioteca/Theos.Biblioteca.Dominio/Permissao/Permissao.cs
@@ -0,0 +1,26 @@
+﻿namespace Theos.Biblioteca.Dominio.Permissao
+{
+    public class Permissao
+    {
+        public bool CadastrarLivro { get; private set; }
+        public bool ExcluirLivro { get; private set; }
+        public bool AlterarLivro { get; private set; }
+        public bool EmprestarLivro { get; private set; }
+        public bool DevolverLivro { get; private set; }
+
+        public void PodeCadastrar() => CadastrarLivro = true;
+        public void PodeExcluir() => ExcluirLivro = true;
+        public void PodeAlterar() => AlterarLivro = true;
+        public void PodeEmprestar() => EmprestarLivro = true;
+        public void PodeDevolver() => DevolverLivro = true;
+
+        public void PodeTudo()
+        {
+            PodeCadastrar();
+            PodeExcluir();
+            PodeAlterar();
+            PodeEmprestar();
+            PodeDevolver();
+        }
+    }
+}
---
+++ Theos.Biblioteca/Theos.Biblioteca.Dominio/Setor/Enumerador/ESetor.cs
@@ -0,0 +1,9 @@
+﻿namespace Theos.Biblioteca.Dominio.Setor.Enumerador
+{
+    public enum ESetor : byte
+    {
+        Desenvolvimento = 1,
+        Teste = 2,
+        Suporte = 3
+    }
+}

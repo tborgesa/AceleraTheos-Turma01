08e77c865fab365e1de9a3a51210095073853001
Thiago <tborgesa@gmail.com>
2018-11-14 07:47:38 -0200

    Aula 13

---
+++ Aula13/UaiQueijos/UaiQueijos.Dominio/Fornecedor/Fornecedor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UaiQueijos.Dominio.Fornecedor
+{
+    public class Fornecedor : Pessoa
+    {
+        public Fornecedor(DateTime dataNascimento, string cpf, string nome)
+            : base(dataNascimento, cpf, nome)
+        {
+        }
+
+        public Fornecedor(DateTime dataNascimento, string cpf, string nome, string endereco)
+            : base(dataNascimento, cpf, nome, endereco)
+        {
+        }
+
+        public override string CriarEmail()
+        {
+            int numero = 9;
+            int numero1 = 0;
+            
+            int result = numero / numero1;
+
+            return "Fornecedor: Feliz Natal e me de desconto ano que vem.";
+        }
+    }
+}
---
+++ Aula13/UaiQueijos/UaiQueijos.Dominio/Funcionario/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UaiQueijos.Dominio.Funcionario
+{
+    public class Funcionario : Pessoa
+    {
+        public Funcionario(DateTime dataNascimento, string cpf, string nome) 
+            : base(dataNascimento, cpf, nome)
+        {
+        }
+
+        public Funcionario(DateTime dataNascimento, string cpf, string nome, string endereco) 
+            : base(dataNascimento, cpf, nome, endereco)
+        {
+        }
+
+        public override string CriarEmail() => "Funcionario: Feliz Natal e descanse bastante para voltar no gás.";
+
+    }
+}

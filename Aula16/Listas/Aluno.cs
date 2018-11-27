c3b82e1ed9f759d4f45f49413fd8bb8a2f2864b6
Thiago <tborgesa@gmail.com>
2018-11-27 20:57:27 -0200

    Aula 16

---
+++ Aula16/Listas/Aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace Listas
+{
+    public class Aluno
+    {
+        public Aluno(int codigo, string nome)
+        {
+            Codigo = codigo;
+            Nome = nome;
+        }
+
+        public int Codigo { get; }
+        public string Nome { get; }
+    }
+}

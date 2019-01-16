f20c1f802eca918a5bbe39a5d6b9a863c7f8ad86
Caroline Molina <carol.molinah@hotmail.com>
2019-01-16 21:01:52 -0200

    Criação do ViewModel, Dto e Repository de ProfessorHorista

---
+++ Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Dominio/Professor/ProfessorHoristaDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theos.SistemaEscolar.Dominio.Professor
+{
+    public class ProfessorHoristaDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DataInsercao { get; set; }
+        public string Nome { get; set; }
+        public string Cpf{ get; set; }
+        public int HorasTrabalhadas { get; set; }
+    }
+}
---
+++ Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Repositorio/ProfessorHoristaRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theos.SistemaEscolar.Dominio.Professor;
+
+namespace Theos.SistemaEscolar.Repositorio
+{
+   public class ProfessorHoristaRepositorio
+    {
+        private List<ProfessorHorista> _professorHorista = new List<ProfessorHorista>();
+
+        public void Inserir(ProfessorHorista professorHorista)
+        {
+             professorHorista.GerarId();
+            _professorHorista.Add(professorHorista);
+        }
+
+        public List<ProfessorHorista> BuscarTodos()
+        {
+            return _professorHorista;
+        }
+
+        public ProfessorHorista BuscarPorId(Guid id)
+        {
+            return _professorHorista.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Atualizar(ProfessorHorista professorHorista)
+        {
+            var professorHoristaLista = _professorHorista.FirstOrDefault(p => p.Id == professorHorista.Id);
+        }
+
+        public void Excluir(Guid id)
+        {
+            _professorHorista = _professorHorista.Where(p => p.Id != id).ToList();
+        }
+    }
+}

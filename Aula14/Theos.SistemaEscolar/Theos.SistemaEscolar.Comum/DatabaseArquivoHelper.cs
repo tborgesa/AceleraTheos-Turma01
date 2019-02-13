eafb12ef23158d09d18c5fc4df691918953d7558
Caroline Molina <carol.molinah@hotmail.com>
2019-02-13 18:04:27 -0200

    Salvar em arquivo

--- Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Comum/DatabaseArquivoHelper.cs
+++ Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Comum/DatabaseArquivoHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
+using Newtonsoft.Json;
+
 
 namespace Theos.SistemaEscolar.Comum
 {
@@ -9,9 +13,48 @@ namespace Theos.SistemaEscolar.Comum
         public static void GravaArquivo(string conteudo, string nomeArquivo)
         {
 
-            //var pasta = ConfigurationManager.AppSettings[""];
+            var pasta = GetPastaConfigurada();
+            var caminhoAbsoluto = $"{pasta}\\{nomeArquivo}.json";
 
+            using (var arquivo = new StreamWriter(caminhoAbsoluto, false))
+                arquivo.Write(conteudo);
         }
 
+        public static List<T> LerArquivo<T>(string nomeArquivo)
+        {
+            var pasta = GetPastaConfigurada();
+            var caminhoAbsoluto = $"{pasta}\\{nomeArquivo}.json";
+
+            if (!File.Exists(caminhoAbsoluto))
+            {
+                var arquivo = File.Create(caminhoAbsoluto);
+                arquivo.Close();
+            }
+
+            string retornoArquivo;
+
+            using (var arquivo = new StreamReader(caminhoAbsoluto))
+                retornoArquivo = arquivo.ReadToEnd();
+
+            return JsonConvert.DeserializeObject<List<T>>(retornoArquivo) ?? new List<T>();
+        }
+
+        private static string GetPastaConfigurada()
+        {
+            //Arquivo config
+
+            var pasta = ConfigurationManager.AppSettings["PastaDataBase"];
+
+            if (string.IsNullOrWhiteSpace(pasta))
+                throw new Exception("Pasta do banco de dados não configurada");
+
+            if (pasta.EndsWith("\\"))
+                pasta = pasta.Remove(pasta.Length - 1);
+
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            return pasta;
+        }
     }
 }
--- Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Repositorio/ProfessorHoristaRepositorio.cs
+++ Aula14/Theos.SistemaEscolar/Theos.SistemaEscolar.Repositorio/ProfessorHoristaRepositorio.cs
@@ -1,40 +1,57 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Theos.SistemaEscolar.Comum;
 using Theos.SistemaEscolar.Dominio.Professor;
 
 namespace Theos.SistemaEscolar.Repositorio
 {
-   public class ProfessorHoristaRepositorio
+    public class ProfessorHoristaRepositorio
     {
-        private List<ProfessorHorista> _professorHorista = new List<ProfessorHorista>();
+        private List<ProfessorHorista> _professoresHorista = new List<ProfessorHorista>();
 
+
+        public ProfessorHoristaRepositorio()
+        {
+            _professoresHorista = DatabaseArquivoHelper.LerArquivo<ProfessorHorista>("ProfessorHorista");
+        }
         public void Inserir(ProfessorHorista professorHorista)
         {
-             professorHorista.GerarId();
-            _professorHorista.Add(professorHorista);
+            professorHorista.GerarId();
+            _professoresHorista.Add(professorHorista);
+           GravarProfessorHoristaArquivo(_professoresHorista);
         }
 
         public List<ProfessorHorista> BuscarTodos()
         {
-            return _professorHorista;
+            return _professoresHorista;
         }
 
         public ProfessorHorista BuscarPorId(Guid id)
         {
-            return _professorHorista.FirstOrDefault(p => p.Id == id);
+            return _professoresHorista.FirstOrDefault(p => p.Id == id);
         }
 
         public void Atualizar(ProfessorHorista professorHorista)
         {
-            var professorHoristaLista = _professorHorista.FirstOrDefault(p => p.Id == professorHorista.Id);
+            var professorHoristaLista = _professoresHorista.FirstOrDefault(p => p.Id == professorHorista.Id);
+            professorHoristaLista = professorHorista;
+            GravarProfessorHoristaArquivo(_professoresHorista);
         }
 
         public void Excluir(Guid id)
         {
-            _professorHorista = _professorHorista.Where(p => p.Id != id).ToList();
+            _professoresHorista = _professoresHorista.Where(p => p.Id != id).ToList();
+
+        }
+
+        private void GravarProfessorHoristaArquivo(List<ProfessorHorista> professoresHorista)
+        {
+            var conteudo = JsonConvert.SerializeObject(professoresHorista);
+            DatabaseArquivoHelper.GravaArquivo(conteudo, "ProfessorHorista");
         }
     }
 }

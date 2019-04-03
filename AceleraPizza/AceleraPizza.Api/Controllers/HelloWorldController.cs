f838e8f27a46212054183c1cfeae9a7847f5d582
Castilho <chrcastilho@gmail.com>
2019-04-02 22:40:42 -0300

    AceleraPizza - Hello World

---
+++ AceleraPizza/AceleraPizza.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace AceleraPizza.Api.Controllers
+{
+    [RoutePrefix("helloworld")]
+    public class HelloWorldController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello World");
+        }
+    }
+}
